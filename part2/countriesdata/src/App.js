a97e576c32d45e47c0b55b4b9668ec0ea50e9cee
CharmingMystic <113670387+CharmingMystic@users.noreply.github.com>
2024-01-10 19:45:10 +0000

    a

--- part2/countriesdata/src/App.js
+++ part2/countriesdata/src/App.js
@@ -3,17 +3,25 @@ import axios from "axios";
 
 import Results from "./components/Results";
 
+// Komponent App, główny komponent aplikacji
 function App() {
+	// Stan lokalny przechowujący wartość wprowadzoną w polu wyszukiwania
 	const [search, setSearch] = useState("");
+	
+	// Stan lokalny przechowujący dane wszystkich krajów
 	const [filteredData, setFilteredData] = useState([]);
+	
+	// Stan lokalny przechowujący dane wszystkich krajów
 	const [allCountriesData, setCountriesData] = useState([]);
 
+	// Efekt uboczny pobierający dane wszystkich krajów przy załadowaniu aplikacji
 	useEffect(() => {
 		axios.get("https://restcountries.com/v3.1/all").then((response) => {
 			setCountriesData(response.data);
 		});
 	}, []);
 
+	// Efekt uboczny filtrujący dane krajów w zależności od wartości wprowadzonej w polu wyszukiwania
 	useEffect(() => {
 		setFilteredData(
 			allCountriesData.filter((country) =>
@@ -22,17 +30,22 @@ function App() {
 		);
 	}, [allCountriesData, search]);
 
+	// Renderowanie komponentu App
 	return (
 		<div className="App">
-			Find Countries:{" "}
+			{/* Pole do wprowadzania tekstu dla wyszukiwania krajów */}
+			Znajdź kraje:{" "}
 			<input
 				type="search"
 				onChange={(e) => setSearch(e.target.value)}
 				value={search}
 			/>
+			
+			{/* Renderowanie komponentu Results i przekazanie mu przefiltrowanych danych i wartości wyszukiwania */}
 			<Results filteredData={filteredData} search={search} />
 		</div>
 	);
 }
 
+// Eksportowanie komponentu App jako domyślnego
 export default App;
--- part2/countriesdata/src/components/SingleCountry.js
+++ part2/countriesdata/src/components/SingleCountry.js
@@ -1,9 +1,11 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 
+// Komponent SingleCountry przyjmuje dane przefiltrowane dla jednego kraju
 function SingleCountry({ filteredData }) {
 	const [weather, setWeather] = useState([]);
 
+	// Efekt uboczny używający API pogody do pobrania informacji o aktualnej pogodzie dla stolicy kraju
 	useEffect(() => {
 		axios(
 			`http://api.openweathermap.org/geo/1.0/direct?q=${filteredData[0].capital}&appid=${process.env.REACT_APP_WEATHER}`
@@ -12,50 +14,67 @@ function SingleCountry({ filteredData }) {
 				const lat = response.data[0].lat;
 				const lon = response.data[0].lon;
 
+				// Drugie zapytanie do API pogody na podstawie współrzędnych geograficznych kraju
 				return axios(
 					`http://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=metric&exclude=minutely,hourly,alerts&appid=${process.env.REACT_APP_WEATHER}`
 				);
 			})
 			.then((response) => {
+				// Ustawienie stanu weather na dane pogodowe z odpowiedzi
 				return setWeather([response.data]);
 			})
 			.catch((err) => {
-				console.log("ERROR: ", err);
+				// Obsługa błędów
+				console.log("BŁĄD: ", err);
 			});
 	}, [filteredData]);
 
 	return (
 		<>
+			{/* Wyświetlenie nazwy kraju */}
 			<h2>{filteredData[0].name.common}</h2>
-			<div>Capital City: {filteredData[0].capital}</div>
-			<div>Area: {filteredData[0].area}</div>
+
+			{/* Informacje o stolicy kraju */}
+			<div>Stolica: {filteredData[0].capital}</div>
+
+			{/* Informacje o obszarze kraju */}
+			<div>Obszar: {filteredData[0].area}</div>
+
+			{/* Informacje o językach kraju */}
 			<div>
-				Languages:
+				Języki:
 				<ul>
 					{Object.entries(filteredData[0].languages).map((arr) => {
 						return <li key={arr[0]}>{arr[1]}</li>;
 					})}
 				</ul>
 			</div>
-			<img src={filteredData[0].flags.png} alt="flag" />
+
+			{/* Wyświetlenie flagi kraju */}
+			<img src={filteredData[0].flags.png} alt="flaga" />
+
+			{/* Warunek sprawdzający, czy dane pogodowe są dostępne */}
 			{weather.length > 0 && (
 				<div>
-					<h3>Weather in {filteredData[0].capital}</h3>
+					{/* Wyświetlenie informacji o pogodzie */}
+					<h3>Pogoda w {filteredData[0].capital}</h3>
 					<div>
-						Temperature {weather[0].current.temp}
+						Temperatura {weather[0].current.temp}
 						<span> &#176;C</span>
 					</div>
 					<div>
+						{/* Wyświetlenie ikony pogodowej */}
 						<img
 							src={`https://openweathermap.org/img/wn/${weather[0].current.weather[0].icon}@4x.png`}
-							alt="weather-icon"
+							alt="ikona-pogody"
 						/>
 					</div>
-					<div>Wind {weather[0].current.wind_speed} m/s</div>
+					<div>Wiatr {weather[0].current.wind_speed} m/s</div>
 				</div>
 			)}
 		</>
 	);
 }
 
+// Eksportowanie komponentu SingleCountry jako domyślnego
 export default SingleCountry;
